677767744bd9444761560957ee3e3c3429559890
jiaxi wang <jwang724@uwo.ca>
2015-02-08 00:39:16 +0000

    built-in

---
+++ asn1.c
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h> 
+#include <unistd.h>
+#include <string.h>
+
+
+#define CMD_SIZE 128
+#define HISTORY_MAX 150
+#define TOKEN_MAX 16
+
+
+typedef enum {false=0, true=1} boolean; 
+
+int main(int argc, char* argv[])
+{
+  char cmd[CMD_SIZE];
+  char cmdHistory[HISTORY_MAX][CMD_SIZE] = {};
+  char* tokens[TOKEN_MAX];
+  int historyIndex = 0, tokenCount;
+  pid_t pid;  
+
+  do{
+  printf("jiaxi>");
+  if (fgets(cmd,CMD_SIZE,stdin) != NULL) 
+  {
+    strtok(cmd, "\n");
+  }
+  else 
+    printf("no command!\n");
+
+//  for (i = 0; i < n; i++)
+//    printf("extracted token is %s\n", tokens[i]);
+
+  //build-in command -- exit
+  if(strcmp(cmd, "exit") == 0)
+  {
+    printf("000\n"); 
+    exit(1);
+  }
+
+  //store the command into the history if it is not "exit"
+  printf("input cmd is %s\n", cmd);
+  strcpy(cmdHistory[historyIndex % HISTORY_MAX], cmd);
+  historyIndex = (historyIndex + 1) % HISTORY_MAX;
+  tokenCount = make_tokenlist(cmd, tokens);
+
+  if(strcmp(tokens[0], "history") == 0)
+  {
+    //outSize is 10 in default
+    int outSize = 10;
+
+    if(tokenCount > 1)
+    {
+      printf("token count is %d\n", tokenCount);
+      outSize =  atoi(tokens[1]);
+    }
+
+    printf("out size is  is %d\n", outSize);
+    history(cmdHistory, historyIndex, outSize);
+  }
+  }while(true);
+}
+
+
+
+int history(char cmdHistory[HISTORY_MAX][CMD_SIZE], int index, int num)
+{
+  printf("index is %d, outsize is %d\n", index, num);
+
+  int count,a;
+  
+  if(num > HISTORY_MAX)
+    num = HISTORY_MAX;
+
+  if(num > index)
+  {
+    num -= index;
+    index--;
+    count = index;
+    if(strcmp(cmdHistory[HISTORY_MAX-1],"") != 0)
+    {
+      count = num;
+      int a;
+      num--;
+      for(a = HISTORY_MAX - 1 - num;num >= 0;count--)
+      {
+        printf("%d %s\n", count, cmdHistory[a++]); 
+      }
+    }
+
+    for(a = 0;index >= 0; index--, count--)
+    {
+      printf("%d %s\n", count, cmdHistory[a++]); 
+    }
+  }
+  else
+  {
+    count = num-1;
+    num--;
+    for(a = 0;num >= 0; num--, count--)
+    {
+      printf("%d %s\n", count, cmdHistory[a++]); 
+    }
+  }
+}
+
+/*
+   This function takes as input the following:
+     buf: This represents the string for which the tokens are to be determined
+          for
+
+     tokens: This represents the array that the found tokens are tbe put into
+
+   The function strtok() is used to find the tokens.  The delimiter used
+   to distinguish tokens is a space
+
+*/
+
+int make_tokenlist(char *buf, char *tokens[])
+{
+
+ char input_line[CMD_SIZE];
+ char *line;
+ int i,n;
+
+ i = 0;
+
+ line =   buf;
+ tokens[i] = strtok(line, " ");
+ do  {
+    i++;
+    line = NULL;
+    tokens[i] = strtok(line, " ");
+ } while(tokens[i] != NULL);
+
+ return i;
+}
+
+
---
+++ asn1.h
@@ -0,0 +1,7 @@
+#ifndef _ASN1_H
+#define _ASN1_H
+
+int history(char cmdHistory[HISTORY_MAX][CMD_SIZE], int index, int num)
+int make_tokenlist(char *buf, char *tokens[])
+
+#endif /* _ASN1_H */
